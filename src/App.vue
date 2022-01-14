6ee4bf128755207307dfc049823b06c512f6ffcd
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-01-13 20:00:55 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <h1 class="text-8xl font-bold underline">Hello world!</h1>
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+html {
+  font-size: 10px;
+}
+</style>
---
+++ src/boot/crestron.js
@@ -0,0 +1,3 @@
+import * as CrComLib from '@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib'
+
+export default { CrComLib }
